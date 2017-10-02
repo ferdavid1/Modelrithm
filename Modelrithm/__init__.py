bcecf868e7178d98d1b609b4df88c53346c21ff6
ferdavid1 <fespinosa@ucdavis.edu>
2017-10-01 17:10:55 -0700

    addded XGBoost

--- Modelrithm/__init__.py
+++ Modelrithm/__init__.py
@@ -6,11 +6,13 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, precision_score, fbeta_score
+from sklearn.grid_search import RandomizedSearchCV
 import matplotlib.pyplot as plt
 import numpy as np
 # from classifier_threading import CThread
 import threading
 import time
+from xgboost import XGBClassifier
 
 class CThread(threading.Thread):
 
@@ -42,7 +44,7 @@ class Modelrithm(object):
 		self.fbeta= []
 		self.classifiernames = ['SupportVectorClassifier', 'KNeighborsClassifier', \
 		'DecisionTreeClassifier', 'RandomForestClassifier', 'AdaBoostClassifier', 'GaussianNaiveBayes',\
-		'LogisticRegression']
+		'LogisticRegression', 'XGBClassifier']
 
 	def Support(self):
 		Xtrain, Xtest, Ytrain, Ytest = self.Xtrain, self.Xtest, self.Ytrain, self.Ytest
@@ -99,6 +101,17 @@ class Modelrithm(object):
 		precision.append(precision_score(Ytest, adaboost.predict(Xtest)))
 		fbeta.append(fbeta_score(Ytest, adaboost.predict(Xtest), beta=1))
 
+	def XGB(self):
+		Xtrain, Xtest, Ytrain, Ytest = self.Xtrain, self.Xtest, self.Ytrain, self.Ytest
+		accuracy = self.accuracy
+		precision = self.precision
+		fbeta = self.fbeta
+		xgb = XGBClassifier()
+		xgb.fit(np.array(Xtrain), np.array(Ytrain))
+		accuracy.append(accuracy_score(Ytest, xgb.predict(Xtest)))
+		precision.append(precision_score(Ytest, xgb.predict(Xtest)))
+		fbeta.append(fbeta_score(Ytest, xgb.predict(Xtest), beta=1))
+
 	def GNB(self):
 		Xtrain, Xtest, Ytrain, Ytest = self.Xtrain, self.Xtest, self.Ytrain, self.Ytest
 		accuracy = self.accuracy
@@ -132,6 +145,7 @@ class Modelrithm(object):
 		adathread = CThread(5, classifiernames[4], self.Ada())
 		gnbthread = CThread(6, classifiernames[5], self.GNB())
 		lrthread = CThread(7, classifiernames[6], self.LogReg())
+		xgbthread = CThread(8, classifiernames[7], self.XGB())
 
 		threads.append(svmthread)
 		threads.append(knthread)
@@ -140,6 +154,7 @@ class Modelrithm(object):
 		threads.append(adathread)
 		threads.append(gnbthread)
 		threads.append(lrthread)
+		threads.append(xgbthread)
 
 		for t in threads:
 			t.start()
@@ -158,25 +173,28 @@ class Modelrithm(object):
 
 		#accuracy.sort()
 		# mostaccurate = accuracy[0]
-		accuracy_dict = {"SVC":accuracy[0], "KNeighborsClassifier": accuracy[1], 'DecisionTreeClassifier':accuracy[2], 'RandomForestClassifier':accuracy[3], 'AdaBoostClassifier':accuracy[4], 'GaussianNB':accuracy[5], 'LogisticRegression':accuracy[6]}
+		accuracy_dict = {"SVC":accuracy[0], "KNeighborsClassifier": accuracy[1], 'DecisionTreeClassifier':accuracy[2], 'RandomForestClassifier':accuracy[3], 'AdaBoostClassifier':accuracy[4], 'GaussianNB':accuracy[5], 'LogisticRegression':accuracy[6], 'XGBClassifier':accuracy[7]}
 		plt.figure()
 		plt.title("Accuracy per Classifier")
-		plt.xticks(np.arange(7), list(accuracy_dict.keys()))
+		plt.xticks(np.arange(len(accuracy_dict)), list(accuracy_dict.keys()))
 		plt.plot(accuracy)
+		plt.tight_layout()
 		plt.show()
 
-		precision_dict = {"SVC":precision[0], "KNeighborsClassifier": precision[1], 'DecisionTreeClassifier':precision[2], 'RandomForestClassifier':precision[3], 'AdaBoostClassifier':precision[4], 'GaussianNB':precision[5], 'LogisticRegression': precision[6]}
+		precision_dict = {"SVC":precision[0], "KNeighborsClassifier": precision[1], 'DecisionTreeClassifier':precision[2], 'RandomForestClassifier':precision[3], 'AdaBoostClassifier':precision[4], 'GaussianNB':precision[5], 'LogisticRegression': precision[6], 'XGBClassifier':precision[7]}
 		plt.figure()
 		plt.title("Precision per Classifier")
-		plt.xticks(np.arange(7), list(precision_dict.keys()))
+		plt.xticks(np.arange(len(precision_dict)), list(precision_dict.keys()))
 		plt.plot(precision)
+		plt.tight_layout()
 		plt.show()
 
-		fbeta_dict = {"SVC": fbeta[0], "KNeighborsClassifier": fbeta[1], 'DecisionTreeClassifier':fbeta[2], 'RandomForestClassifier': fbeta[3], 'AdaBoostClassifier':fbeta[4], 'GaussianNB':fbeta[5], 'LogisticRegression':fbeta[6]}
+		fbeta_dict = {"SVC": fbeta[0], "KNeighborsClassifier": fbeta[1], 'DecisionTreeClassifier':fbeta[2], 'RandomForestClassifier': fbeta[3], 'AdaBoostClassifier':fbeta[4], 'GaussianNB':fbeta[5], 'LogisticRegression':fbeta[6], 'XGBClassifier':fbeta[7]}
 		plt.figure()
 		plt.title("F-Beta per Classifier")
-		plt.xticks(np.arange(7), list(fbeta_dict.keys()))
+		plt.xticks(np.arange(len(fbeta_dict)), list(fbeta_dict.keys()))
 		plt.plot(fbeta)
+		plt.tight_layout()
 		plt.show()
 
 		print("*****")
